a033477ccda4f33a88474823acde0e554833f11e
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-07-11 22:23:40 +0200

    change js player-move function

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 var paper = document.getElementById('paper');
 
-var stone = document.getElementById('stone');
+var rock = document.getElementById('rock');
 
 var scissors = document.getElementById('scissors');
 
@@ -13,7 +13,14 @@ var result = document.getElementById('result');
 var humanScore = 0;
 
 var computerScore = 0;
-//var computerMove = Math.round((Math.random() * 2) + 1);
+
+/*var parms = [
+  parms.actualRund:
+  parms.humanScore:
+  parms.computerScore:
+  parms.points: points
+  endGame:
+]*/
 
 var log = function (text) {
 	output.innerHTML = '<br>' + text + '<br>';
@@ -29,31 +36,55 @@ var won = document.getElementById('won');
 
 var points;
 
+var playerMoveObj = document.querySelectorAll('.player-move')
+
 newGame.addEventListener('click', function(){
   points = window.prompt('To how many points would you like to play?');
   won.innerHTML = 'Wygraj '+ points + ' razy!<br><br>';
   humanScore = 0;
   computerScore = 0;
   paper.disabled = false;
-  stone.disabled = false;
+  rock.disabled = false;
   scissors.disabled = false;
   log ('lets play!');
   score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
 });
 
-paper.addEventListener('click', function () { 
+for(var i = 0; i < playerMoveObj.length; i++){
+  var dataMoveAttribute = playerMoveObj[i].getAttribute('data-move');
+  playerMoveObj[i].addEventListener('click', playerMove.bind(this, playerMoveObj[i]));
+}
+
+function playerMove(dataMove) {
   var computerMove = Math.round((Math.random() * 2) + 1);
+  var computerMoveDictionary = {1: 'paper', 2: 'rock', 3: 'scissors'}
   if(humanScore < points && computerScore < points) {
-    if (computerMove === 1) {
-      log ('REMIS');
+    if (computerMoveDictionary[computerMove] == dataMove.getAttribute('data-move')) {
+      log ('REMIS: you both played ' + dataMove.getAttribute('data-move'));
       score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 2 ) {
-      log ('YOU WON: you played PAPER, computer played ROCK');
+    } else if (computerMove === 1 && dataMove.getAttribute('data-move') == 'rock') {
+      log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
+      computerScore ++;
+      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
+    } else if (computerMove === 3 && dataMove.getAttribute('data-move') == 'rock') {
+      log ('YOU WON: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       humanScore ++;
       score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 3) {
-      log ('YOU LOST: you played PAPER, computer played SCISSORS');
+    } else if (computerMove === 3 && dataMove.getAttribute('data-move') == 'paper') {
+      log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       computerScore ++;
+      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
+    } else if (computerMove === 2 && dataMove.getAttribute('data-move') == 'paper') {
+      log ('YOU WON: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
+      humanScore ++;
+      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
+    } else if (computerMove === 2 && dataMove.getAttribute('data-move') == 'scissors') {
+      log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
+      computerScore ++;
+      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
+    } else if (computerMove === 1 && dataMove.getAttribute('data-move') == 'scissors') {
+      log ('YOU WON: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
+      humanScore ++;
       score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
     }
     if(humanScore == points) {
@@ -64,60 +95,7 @@ paper.addEventListener('click', function () {
       won.innerHTML = 'Wygraj '+ points + ' razy!<br><br>'; 
     }}
   else {
-    paper.disabled = true;
+    dataMove.disabled = true;
   }
-});
 
-stone.addEventListener('click', function () { 
-  var computerMove = Math.round((Math.random() * 2) + 1);
-  if(humanScore < points && computerScore < points) {
-      if (computerMove === 2) {
-      log ('REMIS');
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 3) {
-      log ('YOU WON: you played ROCK, computer played SCISSORS');
-      humanScore ++;
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 1) {
-      log ('YOU LOST: you played ROCK, computer played PAPER');
-      computerScore ++;
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    }
-      if (humanScore == points) {
-      won.innerHTML = 'YOU WON THE ENTIRE GAME!!!';
-    } else if (computerScore == points) {
-      won.innerHTML = 'YOU LOST THE ENTIRE GAME T_T';
-    } else {
-      won.innerHTML = 'Wygraj '+ points + ' razy!<br><br>'; 
-    }}
-  else {
-    stone.disabled = true;
-    }
-});
-
-scissors.addEventListener('click', function () { 
-  var computerMove = Math.round((Math.random() * 2) + 1);
-  if(humanScore < points && computerScore < points) {  
-    if (computerMove === 3) {
-      log ('REMIS');
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 1) {
-      log ('YOU WON: you played SCISSORS, computer played PAPER');
-      humanScore ++;
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    } else if (computerMove === 2) {
-      log ('YOU LOST: you played SCISSORS, computer played ROCK');
-      computerScore ++;
-      score('You: ' + humanScore + '  -  ' + computerScore + ' :Computer');
-    }
-        if (humanScore == points) {
-      won.innerHTML = 'YOU WON THE ENTIRE GAME!!!';
-    } else if (computerScore == points) {
-      won.innerHTML = 'YOU LOST THE ENTIRE GAME T_T';
-    } else {
-      won.innerHTML = 'Wygraj '+ points + ' razy!<br><br>'; 
-    }}
-  else {
-    scissors.disabled = true;
-  }
-});
+}
