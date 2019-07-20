bb6c63c51ddbc7a55985cfebabf4cd0e096e60bc
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-07-20 19:30:16 +0200

    changes in js

--- js/script.js
+++ js/script.js
@@ -35,9 +35,9 @@
 
   var playerMoveObj = document.querySelectorAll('.player-move');
 
-  newGame.addEventListener('click', function(){
+  newGame.addEventListener('click', function() {
     params.points = window.prompt('To how many points would you like to play?');
-    if (isNaN(params.points) || params.points <= 0){
+    if (isNaN(params.points) || params.points <= 0) {
       showModal('is-nan');
     } else {
       won.innerHTML = 'First to score '+ params.points + ' points - wins!<br><br>';
@@ -53,7 +53,7 @@
     }
   });
 
-  for (var i = 0; i < playerMoveObj.length; i++){
+  for (var i = 0; i < playerMoveObj.length; i++) {
     var dataMoveAttribute = playerMoveObj[i].getAttribute('data-move');
     playerMoveObj[i].addEventListener('click', playerMove.bind(this, playerMoveObj[i]));
   }
@@ -63,7 +63,6 @@
   var roundState;
 
   function playerMove(dataMove) {
-    
     if (params.humanScore < params.points && params.computerScore < params.points) {
       gameOn(dataMove);
       params.progress.push(params.actualRound++);
@@ -78,7 +77,7 @@
     }  
   }
 
-  function gameEnd(dataMove){
+  function gameEnd(dataMove) {
     if (params.humanScore == params.points) {
         showModal('game-over');
         scoreModal.innerHTML = 'Congratulations! You WON the game!';
@@ -93,31 +92,31 @@
   }
 
   function gameOn(dataMove){
-    if (computerMoveDictionary[computerMove] == dataMove.getAttribute('data-move')) {
+    if (computerMoveDictionary[computerMove] === dataMove.getAttribute('data-move')) {
       log ('REMIS: you both played ' + dataMove.getAttribute('data-move'));
       roundState = "Remis";
       score('You: ' + params.humanScore + '  -  ' + params.computerScore + ' :Computer');
-    } else if (computerMove === 1 && dataMove.getAttribute('data-move') == 'rock') {
+    } else if (computerMove === 1 && dataMove.getAttribute('data-move') === 'rock') {
       log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       params.computerScore ++;
       roundState = 'Lost';
       score('You: ' + params.humanScore + '  -  ' + params.computerScore + ' :Computer');
-    } else if (computerMove === 3 && dataMove.getAttribute('data-move') == 'rock') {
+    } else if (computerMove === 3 && dataMove.getAttribute('data-move') === 'rock') {
       log ('YOU WON: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       params.humanScore ++;
       roundState = 'Win';
       score('You: ' + params.humanScore + '  -  ' + params.computerScore + ' :Computer');
-    } else if (computerMove === 3 && dataMove.getAttribute('data-move') == 'paper') {
+    } else if (computerMove === 3 && dataMove.getAttribute('data-move') === 'paper') {
       log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       params.computerScore ++;
       roundState = 'Lost';
       score('You: ' + params.humanScore + '  -  ' + params.computerScore + ' :Computer');
-    } else if (computerMove === 2 && dataMove.getAttribute('data-move') == 'paper') {
+    } else if (computerMove === 2 && dataMove.getAttribute('data-move') === 'paper') {
       log ('YOU WON: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       params.humanScore ++;
       roundState = 'Win';
       score('You: ' + params.humanScore + '  -  ' + params.computerScore + ' :Computer');
-    } else if (computerMove === 2 && dataMove.getAttribute('data-move') == 'scissors') {
+    } else if (computerMove === 2 && dataMove.getAttribute('data-move') === 'scissors') {
       log ('YOU LOST: you played ' + dataMove.getAttribute('data-move') + ', computer played ' + computerMoveDictionary[computerMove]);
       params.computerScore ++;
       roundState = 'Lost';
@@ -130,12 +129,12 @@
     }
   }
 
-  var showModal = function(modalID){
+  var showModal = function(modalID) {
     document.querySelector('#modal-overlay').classList.add('show');
     document.querySelector('.modal').classList.remove('show');
     var modals = document.querySelectorAll('.modal');
     for (var i = 0; i < modals.length; i++) {
-      if (modalID == modals[i].id) {
+      if (modalID === modals[i].id) {
         modals[i].classList.add('show');
       } else {
         modals[i].classList.remove('show');
@@ -144,14 +143,14 @@
   };
 
   // adds function that closes the modal 
-  var hideModal = function(event){
+  var hideModal = function(event) {
     event.preventDefault();
     document.querySelector('#modal-overlay').classList.remove('show');
   };
   
   var closeButtons = document.querySelectorAll('.modal .close');
   
-  for (var i = 0; i < closeButtons.length; i++){
+  for (var i = 0; i < closeButtons.length; i++) {
     closeButtons[i].addEventListener('click', hideModal);
   }
   
@@ -161,14 +160,14 @@
   var modals = document.querySelectorAll('.modal');
   
   for (var i = 0; i < modals.length; i++){
-    modals[i].addEventListener('click', function(event){
+    modals[i].addEventListener('click', function(event) {
       event.stopPropagation();
     });
   }
 
   var tableHeaders = ['Round', 'Your move', 'Computer move', 'Win', 'Score']
 
-  var tableConstr = function(){
+  var tableConstr = function() {
     var table =  document.createElement('table');
     var tr = document.createElement('tr');
     for (var i = 0; i < tableHeaders.length; i++){
@@ -179,14 +178,14 @@
     
     table.appendChild(tr);
     
-    for (var i = 0; i < params.progress.length; i++){
-      if(i % 5 == 0){
+    for (var i = 0; i < params.progress.length; i++) {
+      if (i % 5 === 0) {
         tr = document.createElement('tr');
       }
       var td = document.createElement('td');
       td.innerHTML = params.progress[i];
       tr.appendChild(td);
-       if(i % 5 == 4){
+       if (i % 5 === 4) {
         table.appendChild(tr);
       }
     }
